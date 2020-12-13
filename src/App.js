18913a72e9003df5f69a98b170ecf256bd98afd5
Anuj Singla <asingla@redhat.com>
2020-12-13 13:32:43 +0530

    fetch data on user search or state change .

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 // import TraditionalWayFetchData from './TraditionalWayFetchData';
 import { ReactQueryDevtools } from "react-query-devtools";
 // import ReactQueryWay from './ReactQueryWay';
-import Example3 from './Example3';
+// import Example3 from './Example3';
+import Example4 from './Example4';
 
 // https://newsapi.org/
 // 'f02b2a0ecd7a4e41977d296648ad94b7'
@@ -14,7 +15,8 @@ function App() {
         <div>
             {/* <TraditionalWayFetchData /> */}
             {/* <ReactQueryWay /> */}
-            <Example3 />
+            {/* <Example3 /> */}
+            <Example4 />
             <ReactQueryDevtools initialIsOpen />
         </div>
     );
